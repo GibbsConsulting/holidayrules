e3ff279b1de2f2e34f3a497da3b734083de697e5
Mark Gibbs <mark@gibbs.consulting>
2020-06-23 22:42:13 -0700

    Update rules with types

--- holidayrules/rules.py
+++ holidayrules/rules.py
@@ -1,30 +1,30 @@
 """Holiday rules
 """
 
-from abc import abstractmethod
+
 from dataclasses import dataclass
 from datetime import date
-from typing import Any
+from typing import Callable, List, Tuple
 
 
 @dataclass(frozen=True)
 class HolidayRule:
     """A single holiday rule"""
 
-    name : str
-    description : str
-    rule: Any  # Signature is year to (holday, optional extra desc)
+    name: str
+    description: str
+    rule: Callable[[int], Tuple[date, str]]  # Signature is year to (holday, optional extra desc)
 
-    def fix_dates(self, years):
-        """Return an array of holiday dates, one entry per year
+    def fix_dates(self, years: List[int]) -> List[Tuple[date, str]]:
+        """Return an array of holiday dates, one entry per year in the input vector.
 
         Each entry is a datetime.date instance, along with an optional second entry.
-        If the second entry is present, and not None, then its content describes how
+        If the second entry is not None, then its content describes how
         the date differs fron the normal holiday. The canonical use of this is to
         indicate an observed holiday.
         """
         return [self.rule(year) for year in years]
 
-def new_year_no_obs(year):
+def new_year_no_obs(year: int) -> Tuple[date, str]:
     """New year, without any observation"""
     return date(year=year, month=1, day=1), None
--- holidayrules/version.py
+++ holidayrules/version.py
@@ -1,1 +1,3 @@
+"""Version number of package"""
+
 __version__ = '0.0.2'
